91b007d8e31c0e30a17b6fb8775d5e791dca7ce2
Lopo <lopo@losys.sk>
2011-06-05 15:46:53 +0200

    some PresenterFactory tests

---
+++ tests/BailIff/Application/PresenterFactoryTest.php
@@ -0,0 +1,63 @@
+<?php // vim: ts=4 sw=4 ai:
+/**
+ * This file is part of BailIff
+ *
+ * @copyright (c) 2010, 2011 Lopo <lopo@losys.eu>
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License Version 3
+ */
+namespace BailIffTesting\Application;
+
+
+class PresenterFactoryTest
+extends \PHPUnit_Framework_TestCase
+{
+	/** @var Kdyby\Application\PresenterFactory */
+	private $factory;
+
+
+	public function setUp()
+	{
+		$this->factory=new \BailIff\Application\PresenterFactory(new \BailIff\DI\Container);
+	}
+
+	public function testInterface()
+	{
+		$this->assertInstanceOf('\Nette\Application\IPresenterFactory', $this->factory);
+	}
+
+	public function providerPresenterClassPrefix()
+	{
+		return array(
+			array('Foo', 'App\FooPresenter'),
+			array('Foo:Bar', 'App\FooModule\BarPresenter'),
+			array('Foo:Bar:Baz', 'App\FooModule\BarModule\BazPresenter'),
+			array('Foo', 'BailIff\Presenters\FooPresenter', 'fw'),
+			array('Foo:Bar', 'BailIff\Presenters\Foo\BarPresenter', 'fw'),
+			array('Foo:Bar:Baz', 'BailIff\Presenters\Foo\Bar\BazPresenter', 'fw'),
+			array('Foo', 'FooPresenter', 'oof'),
+			array('Foo:Bar', 'Foo\BarPresenter', 'oof'),
+			array('Foo:Bar:Baz', 'Foo\Bar\BazPresenter', 'oof'),
+			);
+	}
+
+	/**
+	 * @dataProvider providerPresenterClassPrefix
+	 */
+	public function testFormatPresenterClass($presenter, $class, $prefix=NULL)
+	{
+		if ($prefix===NULL) {
+			$this->assertEquals($class, $this->factory->formatPresenterClass($presenter), "->formatPresenterClass('$presenter')");
+			}
+		else {
+			$this->assertEquals($class, $this->factory->formatPresenterClass($presenter, $prefix), "->formatPresenterClass('$presenter', '$prefix')");
+			}
+	}
+
+	/**
+	 * @dataProvider providerPresenterClassPrefix
+	 */
+	public function testUnformatPresenterClass($presenter, $class)
+	{
+		$this->assertEquals($presenter, $this->factory->unformatPresenterClass($class), "->unformatPresenterClass('$class')");
+	}
+}
