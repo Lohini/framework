b2afb56edf77b3f7e596061b51ca5d6ef6182ac5
Lopo <lopo@losys.sk>
2011-06-20 19:19:18 +0200

    Configurator update

--- BailIff/common/Configurator.php
+++ BailIff/common/Configurator.php
@@ -1,16 +1,18 @@
-<?php // vim: set ts=4 sw=4 ai:
+<?php // vim: ts=4 sw=4 ai:
 /**
  * This file is part of BailIff
  *
  * @copyright (c) 2010, 2011 Lopo <lopo@losys.eu>
- * @license GNU GPL v3
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License Version 3
  */
 namespace BailIff;
 
 use Nette\DI\IContainer,
 	Nette\DI\Container,
 	Nette\Application\UI\Presenter,
-	Nette\Application\Routers\Route;
+	Nette\Application\Routers\Route,
+	Nette\Environment as NEnvironment,
+	Nette\Caching\Cache;
 
 /**
  * BailIff Configurator
@@ -21,7 +23,9 @@ class Configurator
 extends \Nette\Configurator
 {
 	/** @var array */
-//	public $onCreateContainer=array();
+	public $onBeforeLoadConfig=array();
+	/** @var array */
+	public $onAfterLoadConfig=array();
 
 
 	/**
@@ -30,13 +34,23 @@ extends \Nette\Configurator
 	public function __construct($containerClass='\BailIff\DI\Container')
 	{
 		parent::__construct($containerClass);
+		self::$instance=$this;
+		$container=$this->getContainer();
+		// Back compatibility
+		NEnvironment::setConfigurator($this);
+		NEnvironment::setContext($container);
+
 
 		defined('VAR_DIR') && $this->container->params['varDir']=realpath(VAR_DIR);
 		defined('ROOT_DIR') && $this->container->params['rootDir']=realpath(ROOT_DIR);
 		defined('BAILIFF_DIR') && $this->container->params['bailiffDir']=realpath(BAILIFF_DIR);
 
-		$this->container->robotLoader;
-		//$this->onCreateContainer($this->container);
+		$this->onAfterLoadConfig[]=function(Container $container) {
+			// Load panels
+			if (!$container->params['consoleMode'] && !$container->params['productionMode']) {
+				$container->translatorPanel;
+				}
+			};
 	}
 
 	/**
@@ -112,20 +126,6 @@ extends \Nette\Configurator
 					'presenter' => 'WebLoader',
 					'action' => 'default'
 					));
-		$router[]=new Route('index.php', array(
-					'lang' => $container->getParam('lang', 'en'),
-					'module' => 'Core',
-					'presenter' => 'Default',
-					'action' => 'default',
-					), Route::ONE_WAY);
-
-		$router[]=new Route('<lang>/<presenter>/<action>[/<id>]', array(
-					'lang' => $container->getParam('lang', 'en'),
-					'module' => 'Core',
-					'presenter' => 'Default',
-					'action' => 'default',
-					'id' => NULL
-					));
 
 		return $router;
 	}
@@ -146,8 +146,8 @@ extends \Nette\Configurator
 	public static function createServiceTranslator(IContainer $container)
 	{
 		$translator=new \BailIff\Localization\Translator;
-		$translator->addFile($container->expand(BAILIFF_DIR.'/lang'), 'BailIff');
-		$translator->addFile($container->expand(APP_DIR.'/lang'), 'Application');
+		$translator->addDictionary('BailIff', $container->expand(BAILIFF_DIR.'/lang'));
+		$translator->addDictionary('Application', $container->expand(APP_DIR.'/lang'));
 		return $translator;
 	}
 
@@ -161,22 +161,193 @@ extends \Nette\Configurator
 	}
 
 	/**
+	 * @param \Nette\DI\IContainer $container
+	 * @return \BailIff\Plugins\IManager
+	 */
+	public static function createServicePluginManager(IContainer $container)
+	{
+		return new \BailIff\Plugins\Manager($container);
+	}
+
+	/**
+	 * @param DI\Container $container
+	 * @return Kdyby\Doctrine\Container
+	 */
+	public static function createServiceDoctrine(Container $container)
+	{
+		return new \BailIff\Database\Doctrine\Container($container);
+	}
+
+	/**
 	 * Merges 2nd config into 1st
 	 *
-	 * @param ArrayHash $c1
-	 * @param ArrayHash $c2
-	 * @return Config
+	 * @param array $c1
+	 * @param array $c2
+	 * @return array
 	 */
-	public static function mergeConfigs($c1, $c2)
+	public static function mergeConfigs(array $c1=NULL, array $c2)
 	{
+		if ($c1==NULL) {
+			return $c2;
+			}
 		foreach ($c2 as $k => $v) {
 			if (array_key_exists($k, $c1) && $v!==NULL && (!is_scalar($v) || is_array($v))) {
-				$c1[$k]=self::mergeConfigs($c1->$k, $c2->$k);
+				$c1[$k]=self::mergeConfigs($c1[$k], $c2[$k]);
 				}
 			else {
 				$c1[$k]=$v;
 				}
 			}
 		return $c1;
+	}
+
+	/**
+	 * Loads configuration from file(s) and process it.
+	 * $throws \Nette\InvalidStateException
+	 */
+	public function loadConfig($file, $section=NULL)
+	{
+		$this->onBeforeLoadConfig($container=$this->getContainer());
+		$files= $file===NULL? array($this->defaultConfigFile) : $file;
+		for ($i=0; $i<count($files); $i++) {
+			$files[$i]=$container->expand($files[$i]);
+			}
+		if ($section===NULL) {
+			if (PHP_SAPI==='cli') {
+				$section=NEnvironment::CONSOLE;
+				}
+			else {
+				$section= $container->params['productionMode']? NEnvironment::PRODUCTION : NEnvironment::DEVELOPMENT;
+				}
+			}
+
+		$cache=new Cache($container->templateCacheStorage, 'BailIff.Configurator');
+		$cacheKey=array($files, $section);
+		$cached=$cache->load($cacheKey);
+		if ($cached) {
+			require $cached['file'];
+			fclose($cached['handle']);
+			return;
+			}
+
+		$config=array();
+		foreach ($files as $file) {
+			$config=$this->mergeConfigs($config, \Nette\Config\Config::fromFile($file, $section));
+			}
+
+		$code="<?php\n// source file(s) [".implode(', ', $files)."]\n\n";
+
+		// add expanded variables
+		while (!empty($config['variables'])) {
+			$old=$config['variables'];
+			foreach ($config['variables'] as $key => $value) {
+				try {
+					$code.=$this->generateCode('$container->params[?] = ?', $key, $container->params[$key]=$container->expand($value));
+					unset($config['variables'][$key]);
+					}
+				catch (\Nette\InvalidArgumentException $e) {}
+				}
+			if ($old===$config['variables']) {
+				throw new \Nette\InvalidStateException('Unable to expand variables: '.implode(', ', array_keys($old)).'.');
+				}
+			}
+		unset($config['variables']);
+
+		// process services
+		if (isset($config['services'])) {
+			foreach ($config['services'] as $key => & $def) {
+				if (is_scalar($def)) {
+					$def=array('class' => $def);
+					}
+
+				if (method_exists(get_called_class(), "createService$key")) {
+					$container->removeService($key);
+					if (!isset($def['factory']) && !isset($def['class'])) {
+						$def['factory']=array(get_called_class(), "createService$key");
+						}
+					}
+
+				if (isset($def['option'])) {
+					$def['arguments'][]=$def['option'];
+					}
+
+				if (!empty($def['run'])) {
+					$def['tags']=array('run');
+					}
+				}
+			$builder=new \Nette\DI\ContainerBuilder;
+			$code.=$builder->generateCode($config['services']);
+			unset($config['services']);
+			}
+
+		// expand variables
+		array_walk_recursive($config, function(&$val) {
+			$val=NEnvironment::expand($val);
+			});
+
+		// PHP settings
+		if (isset($config['php'])) {
+			foreach ($config['php'] as $key => $value) {
+				if (is_array($value)) { // back compatibility - flatten INI dots
+					foreach ($value as $k => $v) {
+						$code.=$this->configurePhp("$key.$k", $v);
+						}
+					}
+				else {
+					$code.=$this->configurePhp($key, $value);
+					}
+				}
+			unset($config['php']);
+			}
+
+		// define constants
+		if (isset($config['const'])) {
+			foreach ($config['const'] as $key => $value) {
+				$code.=$this->generateCode('define', $key, $value);
+				}
+			unset($config['const']);
+			}
+
+		// other
+		foreach ($config as $key => $value) {
+			$code.=$this->generateCode('$container->params[?] = '.(is_array($value)? 'Nette\ArrayHash::from(?)' : '?'), $key, $value);
+			}
+
+		// pre-loading
+		$code.=self::preloadEnvironment($container);
+
+		// auto-start services
+		$code .= 'foreach ($container->getServiceNamesByTag("run") as $name => $foo) { $container->getService($name); }' . "\n";
+
+		$cache->save($files, $code, array(
+			Cache::FILES => $files,
+			));
+
+		\Nette\Utils\LimitedScope::evaluate($code, array('container' => $container));
+		$this->onAfterLoadConfig($container);
+	}
+
+	/**
+	 * @param string $statement
+	 * @return string
+	 */
+	private static function generateCode($statement)
+	{
+		$args=func_get_args();
+		unset($args[0]);
+		foreach ($args as &$arg) {
+			$arg=var_export($arg, TRUE);
+			}
+		if (strpos($statement, '?')===FALSE) {
+			return $statement.='('.implode(', ', $args).");\n\n";
+			}
+		$a=strpos($statement, '?');
+		$i=1;
+		while ($a!==FALSE) {
+			$statement=substr_replace($statement, $args[$i], $a, 1);
+			$a=strpos($statement, '?', $a+strlen($args[$i]));
+			$i++;
+			}
+		return $statement.";\n\n";
 	}
 }
